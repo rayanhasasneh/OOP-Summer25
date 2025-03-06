ee1670d4272f25edf02b416ff484295a1f5915dd
rayanhasasneh <rayan.hasasneh@akademiata.edu.pl>
2025-03-06 12:30:25 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-class person_Rayan:
-     x=5
-
-print(person_Rayan)     
+class Person:
+  def __init__(self, first_name, last_name, index_number, nationality):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.index_number = index_number
+    self.nationality = nationality
+p1 =(f"Joe", "Kal", 3456, "indian")   
+p2 =(f"Ali", "Nory", 3498, "amarican") 
+p3 =(f"Ania", "Bos", 3612, "polish")
+print(p1)
+print(p2)
+print(p3)
