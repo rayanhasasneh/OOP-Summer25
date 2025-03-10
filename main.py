6bf3f7468349813b3d4df109e875a9d08a4397fa
rayanhasasneh <rayan.hasasneh@akademiata.edu.pl>
2025-03-10 22:13:46 +0000

    Add Person class

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ class Person:
     self.last_name = last_name
     self.index_number = index_number
     self.nationality = nationality
-p1 =(f"Joe", "Kal", 3456, "indian")   
-p2 =(f"Ali", "Nory", 3498, "amarican") 
-p3 =(f"Ania", "Bos", 3612, "polish")
+p1 =Person("Joe", "Kal", 3456, "indian")   
+p2 =Person("Ali", "Nory", 3498, "amarican") 
+p3 =Person("Ania", "Bos", 3612, "polish")
 print(p1)
 print(p2)
 print(p3)
