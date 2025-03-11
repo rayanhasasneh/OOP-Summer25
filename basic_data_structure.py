a55b078ee1c86719dd83300c8dedb72afe0409ef
rayanhasasneh <rayan.hasasneh@akademiata.edu.pl>
2025-03-11 10:22:09 +0000

    Add Person class

---
+++ basic_data_structure.py
@@ -0,0 +1,8 @@
+my_name = "rayan" #string
+my_age = 20 # integers
+my_birthday = 7.1 #float
+fav_color = ["yallow"] #list
+value ={"number":"name"} #dict
+x = (1, "hi") #tup
+y = {"a","b"} #set
+study = True #bool
