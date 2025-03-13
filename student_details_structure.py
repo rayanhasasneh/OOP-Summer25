8b9b5dc67fab518474493e45a566c5634db89440
rayanhasasneh <rayan.hasasneh@akademiata.edu.pl>
2025-03-13 11:17:55 +0000

    Add Person class

---
+++ student_details_structure.py
@@ -0,0 +1,8 @@
+student ={'first_name' :'Ali' , 'last_name' :'Sam' ,'index_number' :'3728' , 'nationality' : "polish" , 'starting_date' : "01-04-2025",
+           'courses' : ["english" , "programming"]}
+print(student["first_name"])
+print(student["last_name"])
+print(student["index_number"])
+print(student["nationality"])
+print(student["starting_date"])
+print(student["courses"])
