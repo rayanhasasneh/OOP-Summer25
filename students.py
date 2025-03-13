3c3ed63db1bc7d0517e04c06e2d756bb212e0164
rayanhasasneh <rayan.hasasneh@akademiata.edu.pl>
2025-03-13 12:21:20 +0000

    Add Person class

--- students.py
+++ students.py
@@ -4,3 +4,12 @@ students = [{'first_name' :'ali' , 'last_name' :'Sam' ,'index_number' :'3728' , 
            'courses' : ["english" , "arabic","logic"]}]
 for student in students:
     print(f"{student['first_name']},{student['last_name']} - Index: {student['index_number']}")
+
+def add_student(first_name, last_name, index_number, nationality, starting_date, courses):
+    new_student ={'first_name': first_name , 'last_name' : last_name, 'index_number' : index_number, 'nationality' : nationality, 'starting_date' : starting_date,
+           'courses' : courses}
+    students.append(new_student)
+    #print(f"student {first_name} {last_name}")
+add_student('amal', 'kop', '32145', 'american','01-04-2025', ['physics'])
+for student in students:
+    print(f"{student['first_name']},{student['last_name']} - Index: {student['index_number']}")
