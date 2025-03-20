e8f9eeac62c701f3bfd1731addfacfecdfe6aeef
rayanhasasneh <rayan.hasasneh@akademiata.edu.pl>
2025-03-20 11:13:41 +0000

    Add new file

--- animal.py
+++ animal.py
@@ -5,14 +5,13 @@ class Animal:
         self.number_of_legs = number_of_legs
         self.skills = skills 
     def display_info(self):
-        """Returns a formatted string with animal details."""
         return (f"Name: {self.name}, Group: {self.group}, "
                 f"Legs: {self.number_of_legs}, Skills: {', '.join(self.skills)}")
 animals =  [Animal("Dog", "Mammal", 4, ["running", "barking"]),
     Animal("Eagle", "Bird", 2, ["flying", "hunting"]),
-    Animal("Frog", "Amphibian", 4, ["jumping", "swimming"]),
-    Animal("Shark", "Fish", 0, ["swimming", "hunting"]),
-    Animal("Snake", "Reptile", 0, ["slithering", "biting"])]
+    Animal("Frog", "Amphibian", 4, ["jumping"]),
+    Animal("Shark", "Fish", 0, ["swimming"]),
+    Animal("Snake", "Reptile", 0, [ "biting"])]
 
 for animal in animals:
     print(animal.display_info())
